e9c6e43a8fa214e75964010352e912899c34e203
Baasankhuu Dorjbal <baasankhuu.d@unitel.mn>
2022-07-28 19:26:10 +0800

    x

---
+++ src/main/java/mn/unitel/solution/Filter.java
@@ -0,0 +1,20 @@
+package mn.unitel.solution;/*
+ * @created_at 28/07/2022 7:23 PM
+ * @project rasa-gw
+ * @author baasankhuu.d
+ */
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class Filter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext context) {
+      System.out.println(context.getHeaders());
+      System.out.println(context.getUriInfo().getPathParameters());
+    }
+}
