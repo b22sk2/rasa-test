b4cbe950456cff71278d3a74168f763afaae8ab5
khuslen.ba@unitel.mn <khuslen.ba@unitel.mn>
2022-08-04 19:13:46 +0800

    added maintanance mode

--- src/main/java/mn/unitel/solution/Client/RasaClient.java
+++ src/main/java/mn/unitel/solution/Client/RasaClient.java
@@ -8,7 +8,6 @@ import org.jboss.resteasy.reactive.RestHeader;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@RegisterRestClient(baseUri = "http://10.21.68.25:5007/webhooks/rest/webhook")
 public interface RasaClient {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
